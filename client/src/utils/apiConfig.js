6af9bd83ca44b90f8b1dd9f0411829b7d9d50260
unknown <johnmsn22@gmail.com>
2023-06-03 22:16:56 +0100

    base url changed

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,5 +1,6 @@
 const apiConfig = {
-  base_url: "http://localhost:5000/api",
+  // base_url: "http://localhost:5000/api",
+  base_url: "https://sales-app-server.vercel.app/api",
 };
 
 export default apiConfig;
